501bb62ef7936faf0f0e81b9399cd8c8bdad008d
longvoct <volonglqd@gmail.com>
2022-12-25 16:21:28 +0700

    Update svg

--- src/components/layout/HeaderAuth.jsx
+++ src/components/layout/HeaderAuth.jsx
@@ -239,7 +239,7 @@ const HeaderAuth = ({ handleSignOut, userId, ...props }) => {
                                 width="20"
                                 height="20"
                                 fill="currentColor"
-                                class="bi bi-plus-circle-fill"
+                                className="bi bi-plus-circle-fill"
                                 viewBox="0 0 16 16"
                               >
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
@@ -267,7 +267,7 @@ const HeaderAuth = ({ handleSignOut, userId, ...props }) => {
                               width="20"
                               height="20"
                               fill="currentColor"
-                              class="bi bi-person-bounding-box"
+                              className="bi bi-person-bounding-box"
                               viewBox="0 0 16 16"
                             >
                               <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z" />
@@ -288,15 +288,15 @@ const HeaderAuth = ({ handleSignOut, userId, ...props }) => {
                               width="20"
                               height="20"
                               fill="currentColor"
-                              class="bi bi-box-arrow-left"
+                              className="bi bi-box-arrow-left"
                               viewBox="0 0 16 16"
                             >
                               <path
-                                fill-rule="evenodd"
+                                fillRule="evenodd"
                                 d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
                               />
                               <path
-                                fill-rule="evenodd"
+                                fillRule="evenodd"
                                 d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
                               />
                             </svg>
